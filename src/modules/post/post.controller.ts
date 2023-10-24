77d98c20a1719cfc5a54d0c6342ad5c6c8d4278b
Trend20 <enockomondi305@gmail.com>
2023-10-24 15:58:19 +0300

    adding the post controller

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private postService: PostService) {}
+
+  // fetch posts
+  @Get()
+  async getAllPosts() {
+    return await this.postService.getAllPosts();
+  }
+
+  // get post by id
+  @Get('/:id')
+  async getSinglePost(@Param('id') id: string) {
+    return await this.postService.getSinglePost(id);
+  }
+
+  // create post
+  @Post('/')
+  async createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postService.createPost(createPostDto);
+  }
+
+  // update post
+  @Put('/:id')
+  async updatePost(
+    @Param('id') id: string,
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return await this.postService.updatePost(id, updatePostDto);
+  }
+
+  // delete post
+  @Delete('/:id')
+  async deletePost(@Param('id') id: string) {
+    return await this.postService.deletePost(id);
+  }
+}
