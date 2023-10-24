fdb889af1fa844831672624661e560f1dec2bfad
Trend20 <enockomondi305@gmail.com>
2023-10-24 12:41:59 +0300

    adding application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './modules/post/post.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -7,6 +10,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    PostModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
